eb02fd5cd71ba58cc56ca6295b0fc1e57d3efd6a
Corey Frenette <corey@narwhunderful.com>
2016-04-02 12:57:44 -0300

    Use Nom pipeline for compilation; prepend template name to output hashes for easier debugging

--- src/Tailor.php
+++ src/Tailor.php
@@ -1,32 +1,25 @@
 <?php namespace BapCat\Tailor;
 
 use BapCat\Hashing\Hasher;
-use BapCat\Nom\Compiler;
-use BapCat\Nom\Preprocessor;
+use BapCat\Nom\Pipeline;
 use BapCat\Persist\Directory;
 use BapCat\Persist\File;
-use BapCat\Persist\FileReader;
 
 class Tailor {
   private $templates;
-  private $compiled;
-  private $preprocessors = [];
-  private $compiler;
+  private $cache;
+  private $pipeline;
   private $hasher;
   
   private $bindings = [];
   
-  public function __construct(Directory $templates, Directory $compiled, Compiler $compiler, Hasher $hasher) {
-    $this->templates    = $templates;
-    $this->compiled     = $compiled;
-    $this->compiler     = $compiler;
-    $this->hasher       = $hasher;
+  public function __construct(Directory $templates, Directory $cache, Pipeline $pipeline, Hasher $hasher) {
+    $this->templates = $templates;
+    $this->cache     = $cache;
+    $this->pipeline  = $pipeline;
+    $this->hasher    = $hasher;
     
     spl_autoload_register([$this, 'make']);
-  }
-  
-  public function addPreprocessor(Preprocessor $preprocessor) {
-    $this->preprocessors[] = $preprocessor;
   }
   
   public function bind($alias, $template, array $params = []) {
@@ -45,7 +38,7 @@ class Tailor {
   }
   
   private function makeTemplateHash(File $template, array $params) {
-    return $this->hasher->make($template->path . json_encode($params) . $template->modified);
+    return $template->name . '.' . $this->hasher->make($template->path . json_encode($params) . $template->modified);
   }
   
   private function make($alias) {
@@ -55,29 +48,12 @@ class Tailor {
     
     list($template, $params, $hash) = $this->bindings[$alias];
     
-    $compiled_file = $this->compiled->child["$hash.php"];
+    $compiled_file = $this->cache->child["$hash.php"];
     
     if(!$compiled_file->exists) {
-      $code = null;
-      $template->read(function(FileReader $reader) use(&$code) {
-        $code = $reader->read();
-      });
-      
-      $processed_code = $code;
-      
-      foreach($this->preprocessors as $preprocessor) {
-        $processed_code = $preprocessor->process($processed_code);
-      }
-      
-      $processed_file = $this->compiled->child["$hash.php"];
-      
-      //@TODO: use FileWriter when available
-      file_put_contents($processed_file->full_path, $processed_code);
-      
-      //@TODO: this won't work unless it's a LocalFile
-      $compiled_code = $this->compiler->compile($processed_file, $params);
-      $compiled_file = $this->compiled->child["$hash.php"];
-      file_put_contents($compiled_file->full_path, $compiled_code);
+      $compiled_code = $this->pipeline->compile($template, $params);
+      $compiled_file = $this->cache->child["$hash.php"];
+      $compiled_file->writeAll($compiled_code);
     }
     
     if((include $compiled_file->makeLocal()->full_path) == false) {
