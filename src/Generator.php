c58cb69ec449059f6ef5035b0ba3b7dd7411f83b
Corey Frenette <corey@narwhunderful.com>
2019-01-04 08:12:16 -0400

    Update to PHP 7.1

--- src/Generator.php
+++ src/Generator.php
@@ -1,46 +1,69 @@
-<?php namespace BapCat\Tailor;
+<?php declare(strict_types=1); namespace BapCat\Tailor;
 
 use BapCat\Hashing\Algorithms\Sha1WeakHasher;
 use BapCat\Nom\Pipeline;
+use BapCat\Nom\TemplateNotFoundException;
 use BapCat\Persist\Directory;
 use BapCat\Persist\File;
+use BapCat\Persist\NotAFileException;
 
 class Generator {
+  /** @var Directory $templates */
   private $templates;
+
+  /** @var Directory $cache */
   private $cache;
+
+  /** @var Pipeline $pipeline */
   private $pipeline;
+
+  /** @var DefaultHashGenerator $hasher */
   private $hasher;
-  
-  public function __construct(Directory $templates, Directory $cache, Pipeline $pipeline, HashGenerationStrategy $hasher = null) {
+
+  public function __construct(Directory $templates, Directory $cache, Pipeline $pipeline, ?HashGenerationStrategy $hasher = null) {
     $this->templates = $templates;
     $this->cache     = $cache;
     $this->pipeline  = $pipeline;
     $this->hasher    = $hasher ?: new DefaultHashGenerator(new Sha1WeakHasher());
   }
-  
-  public function generate($template, array $params = []) {
+
+  /**
+   * @param  string   $template
+   * @param  mixed[]  $params
+   *
+   * @return  File  The cached compiled template
+   *
+   * @throws  TemplateNotFoundException
+   * @throws  NotAFileException
+   */
+  public function generate(string $template, array $params = []): File {
     $template = $this->templates->child["$template.php"];
-    $hash     = $this->makeTemplateHash($template, $params);
-    
+
+    if(!($template instanceof File)) {
+      throw new NotAFileException($template->path);
+    }
+
+    $hash = $this->makeTemplateHash($template, $params);
+
     $compiled_file = $this->cache->child["$hash.php"];
-    
+
     if(!$compiled_file->exists) {
       $compiled_code = $this->pipeline->compile($template, $params);
       $compiled_file = $this->cache->child["$hash.php"];
       $compiled_file->writeAll($compiled_code);
     }
-    
+
     return $compiled_file;
   }
-  
-  public function includeFile(File $compiled) {
-    if((include $compiled->makeLocal()->full_path) == false) {
+
+  public function includeFile(File $compiled): void {
+    if((include $compiled->makeLocal()->full_path) === false) {
       //@TODO
       throw new \Exception("Could not include [{$compiled->path}]");
     }
   }
-  
-  private function makeTemplateHash(File $template, array $params) {
+
+  private function makeTemplateHash(File $template, array $params): string {
     return $template->name . '.' . $this->hasher->generateHash($template, $params);
   }
 }
--- src/Tailor.php
+++ src/Tailor.php
@@ -1,81 +1,76 @@
-<?php namespace BapCat\Tailor;
+<?php declare(strict_types=1); namespace BapCat\Tailor;
 
-use BapCat\Hashing\Hasher;
 use BapCat\Nom\Pipeline;
 use BapCat\Persist\Directory;
 
 class Tailor {
-  /**
-   * @var  Generator  $generator
-   */
+  /** @var  Generator  $generator */
   private $generator;
-  
-  /**
-   * @var  $bindings  array
-   */
+
+  /** @var  callable[]  $bindings */
   private $bindings = [];
-  
+
   /**
-   * @param  Directory               $templates
-   * @param  Directory               $cache
-   * @param  Pipeline                $pipeline
-   * @param  HashGenerationStrategy  $hasher
+   * @param  Directory                    $templates
+   * @param  Directory                    $cache
+   * @param  Pipeline                     $pipeline
+   * @param  HashGenerationStrategy|null  $hasher
    */
-  public function __construct(Directory $templates, Directory $cache, Pipeline $pipeline, HashGenerationStrategy $hasher = null) {
+  public function __construct(Directory $templates, Directory $cache, Pipeline $pipeline, ?HashGenerationStrategy $hasher = null) {
     $this->generator = new Generator($templates, $cache, $pipeline, $hasher);
-    
+
     spl_autoload_register([$this, 'make']);
   }
-  
+
   /**
    * May be useful for pre-generating classes (see Remodel)
-   * 
+   *
    * @return  Generator
    */
-  public function getGenerator() {
+  public function getGenerator(): Generator {
     return $this->generator;
   }
-  
+
   /**
    * Bind a class to a template with specified parameters
-   * 
-   * @param  string  $alias
-   * @param  string  $template
-   * @param  array   $params    (optional)
-   * 
+   *
+   * @param  string   $alias
+   * @param  string   $template
+   * @param  mixed[]  $params    (optional)
+   *
    * @return  void
    */
-  public function bind($alias, $template, array $params = []) {
-    $this->bindCallback($alias, function(Generator $generator) use($template, $params) {
+  public function bind(string $alias, string $template, array $params = []): void {
+    $this->bindCallback($alias, function(Generator $generator) use($template, $params): void {
       $compiled = $generator->generate($template, $params);
       $generator->includeFile($compiled);
     });
   }
-  
+
   /**
    * Bind a callback to be executed when the specified class is autoloaded
-   * 
+   *
    * @param  string    $alias
    * @param  callable  $generator
-   * 
+   *
    * @return  void
    */
-  public function bindCallback($alias, callable $generator) {
+  public function bindCallback(string $alias, callable $generator): void {
     $this->bindings[$alias] = $generator;
   }
-  
+
   /**
    * Autoloader
-   * 
+   *
    * @param  string  $alias
-   * 
-   * @return  object
+   *
+   * @return  void
    */
-  private function make($alias) {
+  private function make(string $alias): void {
     if(!array_key_exists($alias, $this->bindings)) {
       return;
     }
-    
+
     $this->bindings[$alias]($this->generator);
   }
 }
